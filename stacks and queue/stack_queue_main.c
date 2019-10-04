e8a030a74f1b5e6079ff56b9922e51bb65c10f6f
panipinaka <pinakapani1@gmail.com>
2019-10-04 22:39:04 +0530

    merged stacks and queue

--- stacks and queue/stack_queue_main.c
+++ stacks and queue/stack_queue_main.c
@@ -1,3 +1,4 @@
+#include "queue.h"
 #include "stack.h"
 #include<stdint.h>
 #include<assert.h>
@@ -6,6 +7,59 @@
 #include<stdio.h>
 #include<string.h>
 
+
+
+
+
+
+void test_empty_stack()
+{
+    Stack s = stack_new(5);
+	Stack *stk = &s;
+	assert(stack_full(stk)==0);
+    assert(stack_empty(stk));
+}
+
+void test_two_empty_stack()
+{
+    Stack s = stack_new(2);
+	Stack *stk = &s;
+	Stack_Result res;
+	stk=stack_push(stk,10,&res);
+	stk=stack_peek(stk,&res);
+	assert(res.status==STACK_OK && res.data==10);
+	stk=stack_push(stk,20,&res);
+	assert(stack_full(stk)==1);
+    stk=stack_push(stk,30,&res);
+    assert(res.status==STACK_FULL);
+    stk=stack_pop(stk,&res);
+    stk=stack_pop(stk,&res);
+    stk=stack_pop(stk,&res);
+    assert(res.status==STACK_EMPTY);
+}
+void test_queue()
+{
+   Queue queue = queue_new(5);
+   Queue *q = &queue;
+
+   Queue_Result res;
+
+   q = queue_add(q,10,&res);
+   assert(q->count == 1);
+   assert(q->data[q->head-1] == 10);
+   q = queue_add(q,20,&res);
+   q = queue_add(q,30,&res);
+   assert(q->data[q->head-1]==30);
+   assert(q->data[q->tail]==10);
+
+   q = queue_delete(q,&res);
+   assert(res.data==10);
+   assert(q->data[q->tail]==20);
+}
+
+
+
+//1.balancing of symbols
 int balancing_symbols(char c[10])
 {
 
@@ -47,6 +101,7 @@ int balancing_symbols(char c[10])
 	return 0;
 }
 
+//2.postfix evaluation
 int postfix(char p[20])
 {
 	Stack s = stack_new(5);
@@ -98,42 +153,27 @@ stk = stack_peek(stk,&res);
 assert(res.data=='11');
 
 }
-void test_empty_stack()
-{
-    Stack s = stack_new(5);
-	Stack *stk = &s;
-	assert(stack_full(stk)==0);
-    assert(stack_empty(stk));
-}
 
-void test_two_empty_stack()
-{
-    Stack s = stack_new(2);
-	Stack *stk = &s;
-	Stack_Result res;
-	stk=stack_push(stk,10,&res);
-	stk=stack_peek(stk,&res);
-	assert(res.status==STACK_OK && res.data==10);
-	stk=stack_push(stk,20,&res);
-	assert(stack_full(stk)==1);
-    stk=stack_push(stk,30,&res);
-    assert(res.status==STACK_FULL);
-    stk=stack_pop(stk,&res);
-    stk=stack_pop(stk,&res);
-    stk=stack_pop(stk,&res);
-    assert(res.status==STACK_EMPTY);
-}
 
 int main()
 {
+//stacks
     test_empty_stack();
     test_two_empty_stack();
+
+//1.balancing of symbols
 	char c[10] = "}((]";
 	char c1[20] = "[({})]";
-    //balancing_symbols(c);
+//balancing_symbols(c);
 	balancing_symbols(c1);
+//2.postfix evaluation
 	//char p[20]="235*+6-";
-	postfix(p);
-	return 0;
+	//postfix(p);
+//queue
+    test_queue();
+//4.stacks using queue
+
+    return 0;
+
 
 }
